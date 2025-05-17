0f84b17024fd46bde56129582da5a67363d3a438
NurgaliyevS <nurgaliyev000@gmail.com>
2025-05-17 17:21:35 +0500

    upd

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -79,7 +79,7 @@ function Footer() {
           </svg>
         </Link>
         <Link
-          href="https://www.linkedin.com/in/sabyr-n-43b4a822a/"
+          href="https://www.linkedin.com/in/samat-n-43b4a822a/"
           target="_blank"
           title="Linkedin sabyr-nurgaliyev"
           onClick={() => plausible("Linkedin")}
